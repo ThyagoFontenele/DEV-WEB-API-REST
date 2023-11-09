d182211aca22e4ca590809b4a86f05d89c3b9ced
Thyago Fontenele <thyagofontenele@id.uff.br>
2023-11-09 15:42:52 -0300

    endpoint cliente by id

--- src/main/java/com/devweb/eventoapi/controller/ClientesController.java
+++ src/main/java/com/devweb/eventoapi/controller/ClientesController.java
@@ -3,6 +3,7 @@ package com.devweb.eventoapi.controller;
 import com.devweb.eventoapi.model.Cliente;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class ClientesController {
   
     @GetMapping
     public List<Cliente> getClientes() {
-        return List.of(new Cliente("thyago"));
+        return List.of(new Cliente("thyago", 1));
+    }
+    
+    @GetMapping("/{id}")
+    public Cliente getCliente(@PathVariable(value = "id") int id) {
+        return new Cliente("thyago", id);
     }
 }
--- src/main/java/com/devweb/eventoapi/model/Cliente.java
+++ src/main/java/com/devweb/eventoapi/model/Cliente.java
@@ -2,8 +2,10 @@ package com.devweb.eventoapi.model;
 
 public class Cliente {
     public String nome;
+    public int id;
     
-    public Cliente(String nome) {
+    public Cliente(String nome, int id) {
         this.nome = nome;
+        this.id = id;
     }
 }
