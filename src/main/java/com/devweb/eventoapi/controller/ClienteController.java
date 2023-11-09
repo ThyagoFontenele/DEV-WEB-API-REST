320a08839b123cde89db87f4ddf451dd17ec856b
Thyago Fontenele <thyagofontenele@id.uff.br>
2023-11-09 15:31:11 -0300

    aplicando controller da entidade cliente

---
+++ src/main/java/com/devweb/eventoapi/controller/ClienteController.java
@@ -0,0 +1,17 @@
+package com.devweb.eventoapi.controller;
+
+import com.devweb.eventoapi.model.Cliente;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/cliente")
+public class ClienteController {
+  
+    @GetMapping
+    public List<Cliente> getClientes() {
+        return List.of(new Cliente("thyago"));
+    }
+}
---
+++ src/main/java/com/devweb/eventoapi/model/Cliente.java
@@ -0,0 +1,9 @@
+package com.devweb.eventoapi.model;
+
+public class Cliente {
+    public String nome;
+    
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+}
