27ca9c874485e208baedf081536a3319da2f67f4
Thyago Fontenele <thyagofontenele@id.uff.br>
2023-11-09 21:40:38 -0300

    lint

--- src/main/java/com/devweb/eventoapi/EventoApiApplication.java
+++ src/main/java/com/devweb/eventoapi/EventoApiApplication.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventoApiApplication {
 
     public static void main(String[] args) {
-            SpringApplication.run(EventoApiApplication.class, args);
+        SpringApplication.run(EventoApiApplication.class, args);
     }
 }
