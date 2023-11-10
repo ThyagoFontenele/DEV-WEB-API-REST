f8a8caa80865d08dc1a83a582e4537aa0161548b
Patrick Sasso <patrick.sasso@calindra.com.br>
2023-11-10 20:06:59 -0300

    feat: ✨ enriquece atividades

--- src/main/java/com/devweb/eventoapi/controller/AtividadesController.java
+++ src/main/java/com/devweb/eventoapi/controller/AtividadesController.java
@@ -1,8 +1,11 @@
 package com.devweb.eventoapi.controller;
 
+import java.sql.Time;
+import java.util.Date;
 import java.util.List;
 
 import com.devweb.eventoapi.model.Atividade;
+import com.devweb.eventoapi.model.Local;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,12 +16,28 @@ public class AtividadesController {
     public List<Atividade> getAll() {
         return List.of(new Atividade(
                 1,
-                "atv1")
+                "palestra",
+                "Atividade 1",
+                "primeira palestra",
+                new Date(2023,12,01),
+                new Time(12,30,0),
+                new Time(13,0,0),
+                new Local(1, "teatro")
+                )
         );
     }
 
     @GetMapping("/{id}")
     public Atividade get(@PathVariable(value = "id") int id) {
-        return new Atividade(id, "atv1");
+        return new Atividade(
+                id,
+                "palestra",
+                "Atividade 1",
+                "primeira palestra",
+                new Date(2023,12,01),
+                new Time(12,30,0),
+                new Time(13,0,0),
+                new Local(1, "teatro")
+        );
     }
 }
--- src/main/java/com/devweb/eventoapi/model/Atividade.java
+++ src/main/java/com/devweb/eventoapi/model/Atividade.java
@@ -1,15 +1,37 @@
 package com.devweb.eventoapi.model;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class Atividade {
     public int id;
+    public String tipo;
     public String nome;
+    public Local local;
+    public String descricao;
+    public Date data;
+    public Time horarioInicio;
+    public Time horarioFim;
 
     public Atividade(
             int id,
-            String nome
+            String tipo,
+            String nome,
+            String descricao,
+            Date data,
+            Time horarioInicio,
+            Time horarioFim,
+            Local local
+
+
     ) {
         this.id = id;
+        this.tipo = tipo;
         this.nome = nome;
-
+        this.local = local;
+        this.descricao = descricao;
+        this.data = data;
+        this.horarioInicio = horarioInicio;
+        this.horarioFim = horarioFim;
     }
 }
