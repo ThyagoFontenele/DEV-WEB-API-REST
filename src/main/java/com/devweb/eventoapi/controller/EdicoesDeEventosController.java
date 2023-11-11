539d35d5734e11efc51034e3be9dd17f23a0e700
Patrick Sasso <patrick.sasso@calindra.com.br>
2023-11-10 22:54:31 -0300

    feat: ✨ adiciona edicoes de eventos

--- src/main/java/com/devweb/eventoapi/controller/EdicoesDeEventosController.java
+++ src/main/java/com/devweb/eventoapi/controller/EdicoesDeEventosController.java
@@ -1,27 +1,56 @@
 package com.devweb.eventoapi.controller;
 
+import java.util.Date;
 import java.util.List;
-
-import com.devweb.eventoapi.model.Edicao;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devweb.eventoapi.model.EdicaoDeEvento;
+import com.devweb.eventoapi.model.Organizador;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/edicoes")
-public class EdicoesController {
+public class EdicoesDeEventosController {
 
     @GetMapping
-    public List<Edicao> getAll() {
-        return List.of(new Edicao(
+    public List<EdicaoDeEvento> getAll() {
+        return List.of(new EdicaoDeEvento(
                 1,
-                1)
+                "Meu evento",
+                "novo evento legal",
+                "NEV",
+                2023,
+                1,
+                new Date(2023,12,1),
+                new Date(2023,12,10),
+                "Rio de Janeiro",
+                new Organizador(1, "name")
+                )
         );
     }
 
     @GetMapping("/{id}")
-    public Edicao get(@PathVariable(value = "id") int id) {
-        return new Edicao(id, 1);
+    public EdicaoDeEvento get(@PathVariable(value = "id") int id) {
+        return new EdicaoDeEvento(id,
+                "Meu evento",
+                "novo evento legal",
+                "NEV",
+                2023,
+                1,
+                new Date(2023,12,1),
+                new Date(2023,12,10),
+                "Rio de Janeiro",
+                new Organizador(1, "name")
+        );
     }
+
+    @PostMapping
+    public void post() {
+
+    }
+    @PutMapping
+    public void put() {
+
+    }
+
+    @DeleteMapping
+    public void delete(){}
 }
