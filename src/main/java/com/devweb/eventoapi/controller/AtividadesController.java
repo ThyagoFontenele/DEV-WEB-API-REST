b179bebaec626cb76ad80d8508eeabece50bffce
Patrick Sasso <patrick.sasso@calindra.com.br>
2023-12-15 09:35:36 -0300

    feat: ✨ adiciona crud de atividades

--- src/main/java/com/devweb/eventoapi/controller/AtividadesController.java
+++ src/main/java/com/devweb/eventoapi/controller/AtividadesController.java
@@ -1,12 +1,17 @@
 package com.devweb.eventoapi.controller;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.devweb.eventoapi.entities.Atividade;
-import com.devweb.eventoapi.services.AtividadeService;
+import com.devweb.eventoapi.model.ValidationResult;
 import com.devweb.eventoapi.services.UsuarioService;
+import com.devweb.eventoapi.services.AtividadeService;
 
 @RestController
 @RequestMapping(path = "api/v1/edicoes/{edicaoId}/atividades")
@@ -21,30 +26,85 @@ public class AtividadesController extends AuthController {
 
     @GetMapping
     public List<Atividade> getAll(@PathVariable(value = "edicaoId") Long edicaoId) {
-        return atividadeService.getAll();
+        List<Atividade> list = atividadeService.getAll();
+        List<Atividade> filteredList = new java.util.ArrayList<>(List.of());
+        list.forEach(atividade -> {
+            if (Objects.equals(atividade.edicao.id, edicaoId)) {
+                filteredList.add(atividade);
+            }
+        });
+
+        return filteredList;
     }
 
     @GetMapping("/{id}")
-    public Atividade get(@PathVariable(value = "edicaoId") Long edicaoId,
+    public ResponseEntity get(@PathVariable(value = "edicaoId") Long edicaoId,
                          @PathVariable(value = "id") Long id) {
-        return new Atividade();
+
+        Optional<Atividade> atividade = atividadeService.getById(id);
+
+        if (atividade.isEmpty()){
+            ResponseEntity.notFound().build();
+        }
+
+        if(atividade.isPresent() && !Objects.equals(atividade.get().edicao.id, edicaoId)){
+            ResponseEntity.notFound().build();
+        }
+
+        return atividade.isPresent() ?
+                ResponseEntity.ok(atividade.get()) :
+                ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public void post(@RequestBody Atividade atividade) {
+    public ResponseEntity post(@RequestBody Atividade atividade) {
+        ValidationResult validationResult  = atividadeService.saveOrUpdate(atividade);
 
+        return validationResult.isValid()
+                ? ResponseEntity.ok(atividade)
+                : ResponseEntity.status(validationResult.getStatusCode()).body(validationResult.getErrorMessage());
     }
 
     @PutMapping("/{id}")
-    public void put(@PathVariable(value = "edicaoId") Long edicaoId,
+    public ResponseEntity put(@PathVariable(value = "edicaoId") Long edicaoId,
                     @PathVariable(value = "id") Long id,
                     @RequestBody Atividade atividade) {
 
+        if (id == null) {
+            return ResponseEntity.unprocessableEntity().body("id field is required for this");
+        }
+
+        if (!Objects.equals(atividade.id, id)) {
+            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+        }
+
+        Optional<Atividade> atividadePersistida = atividadeService.getById(atividade.id);
+
+        if (atividadePersistida.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        ValidationResult validationResult = atividadeService.saveOrUpdate(atividade);
+
+        return validationResult.isValid()
+                ? ResponseEntity.ok(atividade)
+                : ResponseEntity.status(validationResult.getStatusCode()).body(validationResult.getErrorMessage());
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable(value = "edicaoId") Long edicaoId,
+    public ResponseEntity delete(@PathVariable(value = "edicaoId") Long edicaoId,
                        @PathVariable(value = "id") Long id) {
-        
+        Optional<Atividade> atividade = atividadeService.getById(id);
+
+        if (atividade.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (!Objects.equals(atividade.get().id, id)) {
+            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+        }
+
+        atividadeService.delete(atividade.get());
+        return ResponseEntity.ok(atividade);
     }
 }
--- src/main/java/com/devweb/eventoapi/services/AtividadeService.java
+++ src/main/java/com/devweb/eventoapi/services/AtividadeService.java
@@ -3,6 +3,9 @@ package com.devweb.eventoapi.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.devweb.eventoapi.entities.Usuario;
+import com.devweb.eventoapi.model.ValidationResult;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.devweb.eventoapi.entities.Atividade;
@@ -15,6 +18,25 @@ public class AtividadeService {
 
     public AtividadeService(AtividadeRepository atividadeRepository) {
        this.atividadeRepository = atividadeRepository;
+    }
+
+    public ValidationResult saveOrUpdate(Atividade atividade) {
+        if (atividade.nome == null) {
+            return new ValidationResult("nome field is required");
+        } else if (atividade.data == null) {
+            return new ValidationResult("data field is required");
+        } else if (atividade.descricao == null) {
+            return new ValidationResult("descricao field is required");
+        } else if (atividade.horarioInicio == null) {
+            return new ValidationResult("horarioInicio field is required");
+        } else if (atividade.horarioFim == null) {
+            return new ValidationResult("horarioFim field is required");
+        } else if (atividade.local == null) {
+            return new ValidationResult("local field is required");
+        }
+
+        this.atividadeRepository.save(atividade);
+        return new ValidationResult();
     }
 
     public Optional<Atividade> getById(Long id) {
