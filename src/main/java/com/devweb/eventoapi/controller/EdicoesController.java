f166f999929fae8cdd3e602a1fccf8795b9b90a6
Patrick Sasso <patrick.sasso@calindra.com.br>
2023-12-18 16:09:12 -0300

    adiciona edicao

--- src/main/java/com/devweb/eventoapi/controller/EdicoesController.java
+++ src/main/java/com/devweb/eventoapi/controller/EdicoesController.java
@@ -1,7 +1,15 @@
 package com.devweb.eventoapi.controller;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+import com.devweb.eventoapi.entities.Atividade;
+import com.devweb.eventoapi.entities.Espaco;
+import com.devweb.eventoapi.model.ValidationResult;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.devweb.eventoapi.entities.Edicao;
@@ -25,20 +33,57 @@ public class EdicoesController extends AuthController {
     }
 
     @GetMapping("/{id}")
-    public Edicao get(@PathVariable(value = "eventoId") int eventoId,
+    public ResponseEntity get(@PathVariable(value = "eventoId") int eventoId,
                       @PathVariable(value = "id") Long id) {
-        return new Edicao ();
+
+        Optional<Edicao> edicao = edicaoService.getById(id);
+
+        if (edicao.isEmpty()){
+            ResponseEntity.notFound().build();
+        }
+
+        return edicao.isPresent() ?
+                ResponseEntity.ok(edicao) :
+                ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public void post(@RequestBody Edicao edicao) {
+    public ResponseEntity post(@RequestBody Edicao edicao) {
+        {
+            ValidationResult validationResult  = edicaoService.saveOrUpdate(edicao);
 
+            return validationResult.isValid()
+                    ? ResponseEntity.ok(edicao)
+                    : ResponseEntity.status(validationResult.getStatusCode()).body(validationResult.getErrorMessage());
+        }
     }
 
     @PutMapping("/{id}")
-    public void put(@PathVariable(value = "eventoId") int eventoId,
+    public ResponseEntity put(@PathVariable(value = "eventoId") int eventoId,
                     @PathVariable(value = "id") Long id, @RequestBody Edicao edicao) {
 
+        if (id == null) {
+            return ResponseEntity.unprocessableEntity().body("id field is required for this");
+        }
+
+        Edicao edicaoPersistida = edicaoService.getById(id).orElse(null);
+
+        if (edicaoPersistida == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        edicaoPersistida.setAno(edicao.ano);
+        edicaoPersistida.setAtividades(edicao.atividades);
+        edicaoPersistida.setEspacos(edicao.espacos);
+        edicaoPersistida.setDataInicial(edicao.dataInicial);
+        edicaoPersistida.setDataFinal(edicao.dataFinal);
+        edicaoPersistida.setCidade(edicao.cidade);
+
+        ValidationResult validationResult = edicaoService.saveOrUpdate(edicaoPersistida);
+
+        return validationResult.isValid()
+                ? ResponseEntity.ok(edicao)
+                : ResponseEntity.status(validationResult.getStatusCode()).body(validationResult.getErrorMessage());
     }
 
     @PatchMapping("/{id}/organizador/{usuarioId}")
@@ -49,8 +94,19 @@ public class EdicoesController extends AuthController {
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable(value = "eventoId") int eventoId,
+    public ResponseEntity delete(@PathVariable(value = "eventoId") int eventoId,
                        @PathVariable(value = "id") Long id) {
+        Optional<Edicao> edicao = edicaoService.getById(id);
 
+        if (edicao.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (!Objects.equals(edicao.get().id, id)) {
+            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+        }
+
+        edicaoService.delete(edicao.get());
+        return ResponseEntity.ok(edicao);
     }
 }
